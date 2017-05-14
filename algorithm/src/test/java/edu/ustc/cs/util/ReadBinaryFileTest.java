84848a4cb2c1c462590909e087f3e324666747d9
ustczyb <ustczyb@mail.ustc.edu.cn>
2017-05-14 09:54:31 +0800

    测试数据集是否正常

--- algorithm/src/test/java/edu/ustc/cs/util/ReadBinaryFileTest.java
+++ algorithm/src/test/java/edu/ustc/cs/util/ReadBinaryFileTest.java
@@ -6,6 +6,9 @@ import org.jgrapht.graph.AbstractBaseGraph;
 import org.junit.Test;
 
 
+import java.io.DataInputStream;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.List;
 
@@ -13,15 +16,48 @@ import java.util.List;
  * Created by zyb on 2017/4/10.
  */
 public class ReadBinaryFileTest {
-//    @Test
-//    public void testFile2Graph() throws IOException {
-//        String path = "E:\\code\\shortpath-query\\generator\\src\\main\\resources\\dataset\\Originalnetwork";
-//        GraphBundle graphBundle = ReadBinaryFileUtil.File2Graph(path);
-//        Nodes nodes = graphBundle.getNodes();
-//        CH ch = new CH(graphBundle.getGraph());
-//        ch.init();
-//        List list = ch.getPathEdges(nodes.get(7442),nodes.get(7416));
-//        System.out.println(list);
-//    }
+    @Test
+    public void testEdgeRead() throws IOException {
+        DataInputStream in = new DataInputStream(new FileInputStream("F:\\code\\Code\\UrbanGen\\dataset\\SanJoaquin\\Originalnetwork.edge"));
+        long id1 = in.readLong();
+        long id2 = in.readLong();
+        byte len = in.readByte();
+        System.out.println(id1);
+        System.out.println(id2);
+        System.out.println(len);
+        if (len > 0) {
+            byte[] data = new byte[len];
+            in.readFully (data);
+            System.out.println(String.valueOf(data));
+            long eID = in.readLong();
+            int eClass = in.readInt();
+            System.out.println(eID);
+            System.out.println(eClass);
+        }
+        else {
+            long eID = in.readLong();
+            int eClass = in.readInt();
+            System.out.println(eID);
+            System.out.println(eClass);
+        }
+    }
 
+    @Test
+    public void testNodeRead() throws IOException {
+        DataInputStream in = new DataInputStream(new FileInputStream("F:\\code\\Code\\UrbanGen\\dataset\\SanJoaquin\\Originalnetwork.node"));
+        byte len = in.readByte();
+        System.out.println(len);
+        if (len > 0) {
+            byte[] data = new byte[len];
+            in.readFully (data);
+            long pID = in.readLong();
+            int x = in.readInt();
+            int y = in.readInt();
+        }
+        else {
+            long pID = in.readLong();
+            int x = in.readInt();
+            int y = in.readInt();
+        }
+    }
 }
