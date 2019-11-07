a4d29115a6ec42b70abd56cb473cadf8f316f439
张育博 <zhangyubo02@meituan.com>
2019-11-07 17:46:34 +0800

    添加注释

--- algorithm/src/main/java/edu/ustc/cs/alg/model/edge/Edge.java
+++ algorithm/src/main/java/edu/ustc/cs/alg/model/edge/Edge.java
@@ -8,12 +8,29 @@ import java.util.List;
  */
 public interface Edge<V> extends Comparable, Serializable{
 
+    /**
+     * 获取另一个顶点
+     * @param v
+     * @return
+     */
     V getAnotherVertex(V v);
 
+    /**
+     * 获取边的两个顶点
+     * @return
+     */
     List<V> getVertexs();
 
+    /**
+     * 获取有向边的起点
+     * @return
+     */
     V getSource();
 
+    /**
+     * 获取有向边的终点
+     * @return
+     */
     V getTarget();
 
     Double getLength();
